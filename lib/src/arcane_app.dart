3c8c1a98fb17fd01d84fb538e8c4d1eee04857f8
Hans Kokx <hans.d.kokx@gmail.com>
2025-04-23 21:57:20 +0200

    Fixes tests and updates reactive theme
    Signed-off-by: Hans Kokx <hans.d.kokx@gmail.com>

--- lib/src/arcane_app.dart
+++ lib/src/arcane_app.dart
@@ -187,17 +187,6 @@ class _ArcaneAppState extends State<ArcaneApp> {
     );
   }
 
-  // Update our context reference whenever the widget is built
-  void _updateContextReference(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      // Only store this context if the widget is still mounted
-      if (mounted) {
-        // Store this context in a way that ArcaneReactiveTheme can access it
-        ArcaneReactiveTheme.I.checkSystemTheme(context);
-      }
-    });
-  }
-
   @override
   void initState() {
     super.initState();
@@ -214,9 +203,17 @@ class _ArcaneAppState extends State<ArcaneApp> {
 
   @override
   void didChangePlatformBrightness() {
-    // This is called when the system brightness changes
-    // Check and update the theme if we're following system theme
-    ArcaneReactiveTheme.I.checkSystemTheme(context);
+    // When system brightness changes, find the current builder context
+    // and use it to check the system theme
+    if (mounted && _appKey.currentContext != null) {
+      // Use the current context from the key to check system theme
+      final BuildContext currentContext = _appKey.currentContext!;
+      if (ArcaneReactiveTheme.I.isFollowingSystemTheme) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          ArcaneReactiveTheme.I.checkSystemTheme(currentContext);
+        });
+      }
+    }
     super.didChangePlatformBrightness();
   }
 }
---
+++ lib/src/services/reactive_theme/reactive_theme_wrapper.dart
@@ -0,0 +1,41 @@
+import "package:arcane_framework/src/services/reactive_theme/reactive_theme_service.dart";
+import "package:flutter/material.dart";
+
+class ArcaneTheme extends InheritedWidget {
+  final ThemeMode themeMode;
+
+  const ArcaneTheme({
+    required this.themeMode,
+    required super.child,
+    super.key,
+  });
+
+  static ArcaneTheme? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ArcaneTheme>();
+  }
+
+  @override
+  bool updateShouldNotify(ArcaneTheme oldWidget) {
+    return oldWidget.themeMode != themeMode;
+  }
+
+  static ArcaneReactiveTheme get service => ArcaneReactiveTheme.I;
+  static bool get isFollowingSystemTheme => service.isFollowingSystemTheme;
+  static Stream<ThemeMode> get currentThemeStream => service.currentThemeStream;
+  static ThemeMode get currentTheme => service.currentTheme;
+  static ThemeMode get systemTheme => service.systemTheme;
+  static ThemeData get dark => service.dark;
+  static ValueNotifier<ThemeData> get darkTheme => service.darkTheme;
+  static ThemeData get light => service.light;
+  static ValueNotifier<ThemeData> get lightTheme => service.lightTheme;
+
+  static ArcaneReactiveTheme Function({ThemeMode? themeMode}) get switchTheme =>
+      service.switchTheme;
+  static ArcaneReactiveTheme Function(BuildContext context)
+      get followSystemTheme => service.followSystemTheme;
+  static void get checkSystemTheme => service.checkSystemTheme;
+  static ArcaneReactiveTheme Function(ThemeData theme) get setDarkTheme =>
+      service.setDarkTheme;
+  static ArcaneReactiveTheme Function(ThemeData theme) get setLightTheme =>
+      service.setLightTheme;
+}
