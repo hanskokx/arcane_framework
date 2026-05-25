26abff3629b3face7552e2948f46d9327ee64e28
Hans Kokx <hans.d.kokx@gmail.com>
2026-05-25 18:53:19 +0200

    test: Add unit tests for authentication and feature flags functionality
    Signed-off-by: Hans Kokx <hans.d.kokx@gmail.com>

---
+++ test/services/authentication/authentication_enums_test.dart
@@ -0,0 +1,22 @@
+import "package:flutter_test/flutter_test.dart";
+import "package:arcane_framework/src/services/authentication/authentication_service.dart";
+
+void main() {
+  group("SignUpStep", () {
+    test("values are correct", () {
+      expect(SignUpStep.confirmSignUp.index, 0);
+      expect(SignUpStep.done.index, 1);
+    });
+  });
+
+  group("AuthenticationStatus", () {
+    test("isAuthenticated returns true only for authenticated", () {
+      expect(AuthenticationStatus.authenticated.isAuthenticated, isTrue);
+      expect(AuthenticationStatus.unauthenticated.isAuthenticated, isFalse);
+    });
+    test("isUnauthenticated returns true only for unauthenticated", () {
+      expect(AuthenticationStatus.authenticated.isUnauthenticated, isFalse);
+      expect(AuthenticationStatus.unauthenticated.isUnauthenticated, isTrue);
+    });
+  });
+}
